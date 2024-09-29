986dcf00df8e049e38ba3f2aa24ad889fb1ac8b6
Mostafa Safaei <m.safaei@Mepcell.com>
2024-09-29 08:26:02 +0330

    third day

--- js/app.js
+++ js/app.js
@@ -9,10 +9,14 @@ const app = Vue.createApp({
             tempAnswer: [],
             // bullet:['A','B','C','D'],
             player: [],
+            score: [],
             currentQuestion: {},
+            clickToStart: false,
             playing: false,
             endGame: false,
-            score: 0,
+            submitedModal: false,
+            playerName: "",
+            totalScore: 0,
             questionCounter: 0,
             maxQuestion: 10,
             tempSelectedAnswer: {},
@@ -36,13 +40,17 @@ const app = Vue.createApp({
         fetch('../db/Player.json')
         .then(res => res.json())
         .then(data => this.player = data)
+
+        fetch('../db/Score.json')
+        .then(res => res.json())
+        .then(data => this.score = data)
     },
 
     methods: {
         startGame(){
             this.playing = true
             this.questionCounter = 0
-            this.score = 0
+            this.totalScore = 0
             this.tempAnswer = []
             this.currentQuestion = this.questions[this.questionCounter]
             for(a in this.answers){
@@ -55,7 +63,7 @@ const app = Vue.createApp({
         },
         nextQuestion(){
             // if(this.questions.lengh === 0 || this.questionCounter >= this.maxQuestion){
-            //     // localStorage.setItem('finalScore', this.score)
+            //     // localStorage.setItem('finaltotalScore', this.totalScore)
             //     // go toend page
             //     this.endGame = true
             // }
@@ -78,12 +86,13 @@ const app = Vue.createApp({
                 if(!this.isEmptyObject(this.tempSelectedAnswer)){
                     this.userAnswers.push(this.tempSelectedAnswer)
                     if(this.tempSelectedAnswer.correct == true){
-                        this.score++
+                        this.totalScore++
                     }
                 }
                 this.tempSelectedAnswer = {}
+                console.log(this.tempSelectedAnswer)
                 console.log(this.userAnswers)
-                console.log(this.score)
+                console.log(this.totalScore)
             }
             else{
                 this.endGame = true
@@ -109,6 +118,52 @@ const app = Vue.createApp({
         },
         isEmptyObject(obj){
             return JSON.stringify(obj) === '{}';
+        },
+        submitQuiz(){
+
+            let ind = null
+            for(p in this.player){
+                ind = this.player[p].id
+            }
+            let newPlayer = {
+                id: ind + 1,
+                name: this.playerName
+            }
+
+            fetch("../db/Player.json" , {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(newPlayer)
+            })
+            console.log(newPlayer)
+
+            let ind1 = null
+            for(s in this.score){
+                ind1 = this.score[s].id
+            }
+
+            for(ua in this.userAnswers){
+                let newScore = {
+                    id: ind1 + 1,
+                    playerId: ind + 1,
+                    answerId: this.userAnswers[ua].id
+                }
+                // fetch('../db/Score.json', {
+                //     method: 'POST',
+                //     headers: {'Content-Type': 'application/json'},
+                //     body: JSON.stringify(newScore)
+                // })
+                console.log(newScore)
+                ind1++
+            }
+
+            this.submitedModal = true
+
+            setTimeout(() => {
+                this.submitedModal = false
+            }, 2500);
+
+            this.playerName = ""
         }
     },
 })
